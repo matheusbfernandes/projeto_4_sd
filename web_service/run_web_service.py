e8fa60acc8613f1dd30abf8c843034cb5b6b5c99
Matheus Bento Fernandes <matheus.bento.fernandes@gmail.com>
2019-06-22 22:53:21 -0300

    mudando a arganização dos arquivos

--- web_service/run_web_service.py
+++ web_service/run_web_service.py
@@ -1,15 +1,17 @@
+import base64
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource, Api
-import base64
+
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self, img):
-        # Transformando em imagem e salvando no pc
+
+class Communicator(Resource):
+    @staticmethod
+    def get(img):
         img = img.replace("|", "/")
         img_data = bytes(img, 'utf-8')
         with open("imageToSave.png", "wb") as fh:
@@ -17,7 +19,8 @@ class HelloWorld(Resource):
         return {'img': 'ok'}
 
 
-api.add_resource(HelloWorld, '/<string:img>')
+api.add_resource(Communicator, '/<string:img>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
